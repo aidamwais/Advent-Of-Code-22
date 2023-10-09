8b72d141b4c1697b0578fb0d6b4c31ac0f01f737
aidamwais <39023994+aidamwais@users.noreply.github.com>
2023-10-09 18:27:34 +0300

    Add files via upload

--- Advent Of Code 22/Day 1.hpp
+++ Advent Of Code 22/Day 1.hpp
@@ -3,11 +3,17 @@
 //
 
 /**
+ * Part 1:
  * The Elves take turns writing down the number of Calories contained by the various meals,
  * snacks, rations, etc. that they've brought with them, one item per line.
  * Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
  *
  * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+ *
+ *
+ * Part 2:
+ * Find the top three Elves carrying the most Calories.
+ * How many Calories are those Elves carrying in total?
  */
 #ifndef ADVENT_OF_CODE_22_DAY_1_HPP
 #define ADVENT_OF_CODE_22_DAY_1_HPP
